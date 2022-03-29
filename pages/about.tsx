62fcd0833c5ffd0cc0b27cfaf6c9602a9342fefa
johir-rayhan <johir.rayhan01@gmail.com>
2022-03-29 10:51:54 +0600

    About section done

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,9 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import aboutImg from '../assets/images/about-img.jpeg'
+import Header from '../component/header'
+import styles from '../styles/About.module.css'
 
 const About: NextPage = () => {
   return (
@@ -7,6 +11,22 @@ const About: NextPage = () => {
       <Head>
         <title>Responsive Coffee Shop About Page</title>
       </Head>
+      {/* Header menu */}
+      <Header />
+      <section className={styles.about}>
+        <h1 className='heading'><span className="span">About</span> us</h1>
+        <div className={styles.row}>
+          <div className={styles.image}>
+            <Image src={aboutImg} alt='about' />
+          </div>
+          <div className={styles.content}>
+            <h3 className={styles.title}>what make our coffee special?</h3>
+            <p className={styles.text}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s </p>
+            <p className={styles.text}>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+            <a href='/about-more' className='btn'>learn more</a>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
