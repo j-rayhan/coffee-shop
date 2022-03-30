0a9a806508052b9749697cac15b14f047711b718
johir-rayhan <johir.rayhan01@gmail.com>
2022-03-30 10:06:17 +0600

    Blog section done

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,14 +1,56 @@
-import { NextPage } from 'next'
 import Head from 'next/head'
-
-const Blog: NextPage = () => {
+import withHeaderMenu from '../component/withMenu'
+import blog1 from '../assets/images/blog-1.jpeg'
+import blog2 from '../assets/images/blog-2.jpeg'
+import blog3 from '../assets/images/blog-3.jpeg'
+import styles from '../styles/Blog.module.css'
+import Image, { StaticImageData } from 'next/image'
+type blog = {
+  key: string,
+  image: StaticImageData
+}
+const blogList: blog[] = [
+  {
+    key: '1',
+    image: blog1
+  },
+  {
+    key: '2',
+    image: blog2
+  },
+  {
+    key: '3',
+    image: blog3
+  },
+]
+const Blog: React.FC = () => {
   return (
     <div>
       <Head>
         <title>Responsive Coffee Shop Blog Page</title>
       </Head>
+      <section className={styles.blog}>
+        <h1 className="heading"> our <span className='span'>blogs</span></h1>
+        <div className={styles.boxContainer}>
+          {
+            blogList.map(({ key, image }) => (
+              <div key={key} className={styles.box}>
+                <div className={styles.image}>
+                  <Image src={image} alt='blog' />
+                </div>
+                <div className={styles.content}>
+                  <a href='https://' className={styles.title}>testy and refresh coffee</a>
+                  <span className={styles.span}> by admin / 21st may, 2022</span>
+                  <p className={styles.text}>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+                  <a href='#' className='btn'>read more</a>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </section>
     </div>
   )
 }
 
-export default Blog;
+export default withHeaderMenu(Blog);
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -13,7 +13,7 @@ const Contact: React.FC = () => {
       <section className={styles.contact}>
         <h1 className="heading"> contact <span className='span'>us</span></h1>
         <div className={styles.row}>
-        <iframe className={styles.map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.679349658889!2d90.43126031469102!3d23.829998691648306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c63f1ea325a3%3A0x1d12e560678ff689!2z4KaW4Ka_4Kay4KaV4KeN4Ka34KeH4KakIOCmleCmrOCmsOCmuOCnjeCmpeCmvuCmqA!5e0!3m2!1sbn!2sbd!4v1648552041750!5m2!1sbn!2sbd" allowFullScreen loading="lazy" referrerpolicy="no-referrer-when-downgrade" />
+        <iframe className={styles.map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3649.679349658889!2d90.43126031469102!3d23.829998691648306!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c63f1ea325a3%3A0x1d12e560678ff689!2z4KaW4Ka_4Kay4KaV4KeN4Ka34KeH4KakIOCmleCmrOCmsOCmuOCnjeCmpeCmvuCmqA!5e0!3m2!1sbn!2sbd!4v1648552041750!5m2!1sbn!2sbd" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
         <form action='' className={styles.form}>
           <h3 className={styles.title}>get in touch</h3>
           <div className={styles.inputBox}>
